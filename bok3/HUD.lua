e1d54b3569168ca29ba710fecfdecf096d32b214
xDrHellx <17726521+xDrHellx@users.noreply.github.com>
2023-07-31 21:05:26 +0200

    Added basic files
    Added basic files for the interface, however a lot of code still needs to be done

---
+++ bok3/HUD.lua
@@ -0,0 +1,19 @@
+-- HUD script for Boktai 3, compatible with the english translation patch
+
+local hud = {};
+
+local utilities = dofile("all/utilities.lua");
+
+-- Initializes the HUD
+function hud.initialize()
+
+	-- Get list of memory addresses
+	hud.addresses = require("bok3/addresses");
+
+	-- Starts recording inputs
+	hud.buttons = require("bok3/buttons");
+
+	-- Initializes interface shown on screen
+end;
+
+return hud;
---
+++ bok_HUD.lua
@@ -0,0 +1,38 @@
+-- Main HUD script for GBA Boktai games
+
+console.clear();
+
+print("ROM Hash: " .. gameinfo.getromhash());
+print("ROM Name: " .. gameinfo.getromname());
+print("Game Info: " ..gameinfo.getboardtype());
+
+-- Loads corresponding HUD based on which game is running
+function load_HUD()
+
+    local code = bit.band(memory.read_u32_le(0x080000AC), 0xFFFFFF);
+
+    if(code == 4797269) then
+        return dofile("bok1/HUD.lua");
+    elseif(code == 3289941) then
+        return dofile("bok2/HUD.lua");
+    elseif(code == 3355477) then
+        return dofile("bok3/HUD.lua");
+    end;
+
+    return nil;
+end;
+
+local hud = load_HUD();
+
+if hud then
+    hud.initialize();
+    while true do
+        -- hud.update();
+        emu.frameadvance();
+    end
+else
+    print("\nGame not recognized!\n");
+    while true do
+        emu.frameadvance(); -- idle until a new ROM is loaded
+    end
+end
