a7c9ea1bfcef8a4065e44ae36a5e20fba7672db8
xDrHellx <17726521+xDrHellx@users.noreply.github.com>
2023-08-01 21:43:46 +0200

    Added initialize and addresses file for Bok 2
    Added hud.initialize function and created the file that will contain memory addresses

---
+++ bok2/HUD.lua
@@ -0,0 +1,19 @@
+-- HUD script for Boktai 2
+
+local hud = {};
+
+local utilities = dofile("all/utilities.lua");
+
+-- Initializes the HUD
+function hud.initialize()
+
+	-- Get list of memory addresses
+	hud.addresses = require("bok2/addresses");
+
+	-- Starts recording inputs
+	-- hud.buttons = require("bok2/buttons");
+
+	-- Initializes interface shown on screen
+end;
+
+return hud;
---
+++ bok2/addresses.lua
@@ -0,0 +1,11 @@
+-- List of memory addresses
+-- Note : 	Due to how the game works, if values are modified,
+-- 			the game will only use the new values after switching rooms
+--			or reloading the room
+
+local addresses = {};
+
+
+
+-- Returns addresses
+return addresses;
