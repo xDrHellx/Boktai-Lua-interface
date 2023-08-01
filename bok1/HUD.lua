23c44b8c785cd6924af80bec9b2aad6ce95f4c0a
xDrHellx <17726521+xDrHellx@users.noreply.github.com>
2023-08-01 21:43:33 +0200

    Added initialize and addresses file for Bok 1
    Added hud.initialize function and created the file that will contain memory addresses

---
+++ bok1/HUD.lua
@@ -0,0 +1,19 @@
+-- HUD script for Boktai 1
+
+local hud = {};
+
+local utilities = dofile("all/utilities.lua");
+
+-- Initializes the HUD
+function hud.initialize()
+
+	-- Get list of memory addresses
+	hud.addresses = require("bok1/addresses");
+
+	-- Starts recording inputs
+	-- hud.buttons = require("bok1/buttons");
+
+	-- Initializes interface shown on screen
+end;
+
+return hud;
