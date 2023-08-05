fbaa89e80458b41c2b982c42f06c263675342f01
xDrHellx <17726521+xDrHellx@users.noreply.github.com>
2023-08-05 19:06:49 +0200

    Updated utilities
    Added an "isEmpty" function (based on PHP's empty function) for verifying if a value is empty (ie if it's nil, an empty string, 0 or false)
    
    Updated the "getValueFromMemory" function for better handling domain of passed memory addresses. Added a new optional parameter "readType" used for setting the function to use for getting the value from the memory address (for example "u32_le", "s16_le", ....)

--- all/utilities.lua
+++ all/utilities.lua
@@ -2,6 +2,14 @@
 
 local utilities = {};
 
+-- Verifies if a value is empty (nil, empty string, 0, false)
+-- Based on PHP's empty() function
+-- @param mixed value Value to verify
+-- @return bool True | False if not nil and not empty string
+function utilities.isEmpty(value)
+	return value == nil or value == '' or value == 0 or value == false;
+end;
+
 -- Dumps data, useful for tables within tables
 -- @param mixed data Data to dump
 -- @param bool showAsArray True if print should look like arrays, other will be like JSON objects
@@ -41,13 +49,35 @@ end;
 
 -- Retrieve value from a memory address
 -- @param string address Memory address
--- @param string domain Domain
+-- @param string domain Memory domain
+-- @param string readType Type used for reading the address (u32_le, s16_le, ...)
 -- @return mixed Value
-function utilities.getValueFromMemory(address, domain)
+function utilities.getValueFromMemory(address, domain, readType)
 
-	-- Handling optional parameters	& returning value
-	domain = (domain and domain or 0xFFFFFF);
- 	return memory.read_u32_le(address) & domain;
+	-- Handling optional parameters
+	if (utilities.isEmpty(domain) == false) then
+
+		-- If a domain is passed, add a comma and quotes
+		-- because it will be passed as a second parameter in the function
+		domain = ', "'..domain..'"';
+	else
+		domain = '';
+	end;
+
+	if (readType == nil or readType == '') then
+		readType = 'u32_le';
+	end;
+
+	-- Set the the name & parameters for the function to use to get the value from the memory address
+	local readingFunction = 'return memory.read_'..readType..'('..address..domain..')';
+
+	-- Execute the function & return the value
+	-- If a domain was passed, do not do the bitwise operation
+	if (utilities.isEmpty(domain) == false) then
+		return load(readingFunction)();
+	else
+ 		return load(readingFunction)() & 0xFFFFFF;
+	end;
 end;
 
 return utilities;
